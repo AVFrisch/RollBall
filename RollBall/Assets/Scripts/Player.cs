7874c529c0b05c7ed921085a2d811b5d36ac363a
ADS\avfrisch <ahmed.v.frisch@gmail.com>
2019-02-06 12:34:21 -0500

    End of class commit on 2/6

---
+++ RollBall/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //
+    private void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+        Debug.Log(move);
+    }
+
+    
+}
