f0a7c71dc50f68a7afb40fe5d05ca5c1743404e0
ADS\avfrisch <ahmed.v.frisch@gmail.com>
2019-02-11 11:56:53 -0500

    Pickups can now be collected by colliding with them

--- RollBall/Assets/Scripts/Player.cs
+++ RollBall/Assets/Scripts/Player.cs
@@ -1,30 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 1;
-	
-	private Rigidbody rb;
-	
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
-	
-    //
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void FixedUpdate()
     {
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
-        //Debug.Log(move);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce (move * speed);
+        rb.AddForce(movement * speed);
     }
 
-    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
