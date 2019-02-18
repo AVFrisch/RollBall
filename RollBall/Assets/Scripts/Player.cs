55a5d2d806f5c0cd773b34bb3b4096e0f7677380
AVFrisch <ahmed.v.frisch@gmail.com>
2019-02-18 02:17:08 -0500

    Every time I switch machines something breaks. My slopes and jumping are now gone and the ball just bumps into the pickups instead even though they're properly tagged. I'm keeping USB backups after this since Sourcetree has consistently been setting me back days at a time.

--- RollBall/Assets/Scripts/Player.cs
+++ RollBall/Assets/Scripts/Player.cs
@@ -43,6 +43,7 @@ public class Player : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+
         if (count >= 9)
         {
             winText.text = "You Win!";
