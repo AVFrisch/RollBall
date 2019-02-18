661917704f7e838e698535192e4b16b165514d31
ADS\avfrisch <ahmed.v.frisch@gmail.com>
2019-02-18 12:05:22 -0500

    Attempting to re-add jumping and dashing

--- RollBall/Assets/Scripts/Player.cs
+++ RollBall/Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
 
     public float speed;
+    public float dashSpeed = 50;
     public Text countText;
     public Text winText;
+    public float dashCool = 30;
 
     private Rigidbody rb;
     private int count;
@@ -26,10 +28,35 @@ public class Player : MonoBehaviour
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        //bool moveDash = Input.GetAxis("Fire3");
+        float moveJump = Input.GetAxis("Jump");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(movement * speed * dashSpeed);
+            //dashCool = 30;
+        }
+
+        if (dashCool > 0)
+        {
+            dashCool -= 1;
+        }
+
+        if (dashCool < 0)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb.AddForce(movement * speed * dashSpeed);
+                dashCool = 30;
+            }
+        }
+
+
+        
     }
 
     void OnTriggerEnter(Collider other)
